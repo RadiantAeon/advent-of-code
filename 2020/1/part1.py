b52a28b06d670d7aa766190bcb53002516fbd4a5
RealAwesomeness <RealAwesomeness@outlook.com>
2020-12-07 19:28:49 -0500

    day 1

---
+++ 2020/1/part1.py
@@ -0,0 +1,13 @@
+import sys
+# split the puzzle input by newline
+puzzle_input = open("input", "r").read().strip().split("\n")
+# strip any remaining whitespace and turn each value into an int
+puzzle_input = [int(e.strip()) for e in puzzle_input]
+
+# loop through all values to get desired output
+for num1 in puzzle_input:
+    for num2 in puzzle_input:
+        if num1 + num2 == 2020:
+            # print out the product and exit
+            print(num1 * num2)
+            sys.exit(0)
---
+++ 2020/1/part2.py
@@ -0,0 +1,14 @@
+import sys
+# split the puzzle input by newline
+puzzle_input = open("input", "r").read().strip().split("\n")
+# strip any remaining whitespace and turn each value into an int
+puzzle_input = [int(e.strip()) for e in puzzle_input]
+
+# loop through all values to get desired output
+for num1 in puzzle_input:
+    for num2 in puzzle_input:
+        for num3 in puzzle_input:
+            if num1 + num2 + num3 == 2020:
+                # print out the product and exit
+                print(num1 * num2 * num3)
+                sys.exit(0)
